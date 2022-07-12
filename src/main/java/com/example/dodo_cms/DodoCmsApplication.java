4f994c9b70368b843f8f7f5c3bbc3915da156a81
dohee <ododalrio@gmail.com>
2022-07-12 15:54:14 +0900

    start

---
+++ src/main/java/com/example/dodo_cms/DodoCmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.dodo_cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DodoCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DodoCmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dodo_cms/DodoCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dodo_cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DodoCmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
